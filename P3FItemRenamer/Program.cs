85d32ab3649b720f72ef2d58d98e819644a63b41
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2022-01-27 19:16:24 +1000

    Add exit messages

--- P3FItemRenamer/Program.cs
+++ P3FItemRenamer/Program.cs
@@ -13,3 +13,7 @@ originalItems.AddRange(NameListConverter.ConvertFile(Path.Combine(basePath, "Boo
 originalItems.AddRange(NameListConverter.ConvertFile(Path.Combine(basePath, "Accessories.CEM"), 0x205E1068));
 ItemRenamer itemRenamer = new ItemRenamer(originalItems);
 itemRenamer.Start();
+Console.WriteLine("Thanks for using my pnach creator. To use this pnach simply copy it to your cheats folder in PCSX2 and play!");
+Console.WriteLine("All of the renamed items have their new names as comments above their patch so if you'd like to disable any one you can comment out the patch lines below it.");
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
